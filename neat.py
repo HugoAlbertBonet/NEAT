ab8d2c9a8da9fe9aede26f003b15e6da90e7c22c
Hugo Albert <hugoalberthlu@gmail.com>
2024-08-10 18:44:49 +0200

    mutations implemented

--- neat.py
+++ neat.py
@@ -25,6 +25,24 @@ class Node:
     def __init__(self, node_id, node_type):
         self.id = node_id
         self.type = node_type
+
+class Network:
+    def __init__(self, inputs, outputs):
+        self.nodes = []
+        self.connections = []
+        self.connections_set = set()
+        self.inputs = inputs
+        self.outputs = outputs
+        self.hidden = 0
+        for i in range(inputs):
+            self.nodes.append(Node(i, NodeType.INPUT))
+        for i in range(outputs):
+            self.nodes.append(Node(len(self.nodes), NodeType.OUTPUT))
+            for j in range(inputs):
+                self.connections.append(Connection(len(self.connections), self.nodes[j], self.nodes[-1], random.uniform(-1,1)))
+                self.connections_set.add((j, len(self.nodes)-1))
+
+        
     
 
 class NEAT:
@@ -61,13 +79,22 @@ class NEAT:
             if parent.connections[i].enabled:
                 break
         parent.nodes.append(Node(len(parent.nodes), NodeType.HIDDEN))
+        parent.hidden += 1
         parent.connections[i].disable()
         parent.connections.append(Connection(len(parent.connections), parent.connections[i].node_in, parent.nodes[-1], weight=random.uniform(-1,1)))
+        parent.connections_set.add((parent.connections[i].node_in.index, parent.nodes[-1].index))
         parent.connections.append(Connection(len(parent.connections), parent.nodes[-1], parent.connections[i].node_out, weight=random.uniform(-1,1)))
+        parent.connections_set.add((parent.nodes[-1].index, parent.connections[i].node_out.index))
 
 
     def mutation_connection(self, parent):
-        pass
+        while True:
+            a, b = random.randint(0, len(parent.nodes)-1), random.randint(0, len(parent.nodes))
+            if a != b and (a, b) not in parent.connections_set:
+                break
+        parent.connections.append(Connection(len(parent.connections), parent.nodes[a], parent.nodes[b], weight=random.uniform(-1,1)))
+        parent.connections_set.add((a, b))
 
     def mutation_weight(self, parent):
-        pass
+        i = random.randint(0, len(parent.connections) - 1)
+        parent.connections[i].weight = random.uniform(-1,1)
